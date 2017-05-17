992ac6532042a1b5be5f77fedc7217d36bed614d
David Brechin <dbrechin@live.ca>
2017-05-16 20:15:30 -0400

    Loops challenge

---
+++ ingredients.js
@@ -0,0 +1,25 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+var content = 7;
+while (content >= 0) {
+  console.log(ingredients[content]);
+  content = content - 1;
+}
+// Write a while loop that prints out the contents of ingredients:
+var content = 0;
+while (content < ingredients.length) {
+  console.log(ingredients[content]);
+  content++;
+}
+// Write a for loop that prints out the contents of ingredients:
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(packingList[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+var content = 7;
+while (content >= 0) {
+  console.log(ingredients[content]);
+  content = content - 1;
+}
+
